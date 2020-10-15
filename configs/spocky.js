cbca5208c4ffd0d02bc179c3590ec346f07195e0
SfTd <j.zolcik@allblue.pl>
2020-10-15 11:12:10 +0200

    Development.

--- configs/spocky.js
+++ configs/spocky.js
@@ -6,6 +6,7 @@ module.exports.init = function(conf, data)
     require('./ab-layouts').init(conf, data);
     require('./ab-nodes').init(conf, data);
     require('./ab-strings').init(conf, data);
+    require('./ab-text-parser').init(conf, data);
     
     conf.extObj(data['js-libs'].libs, {
         'spocky': '../dev/node_modules/spocky/js-lib',
