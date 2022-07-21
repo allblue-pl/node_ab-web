73fdfbf4bde099221540a9e22c1ce9c4a2745d97
AllBlue Monkey <j.zolcik@allblue.pl>
2022-07-21 09:02:36 +0200

    Development.

---
+++ configs/e-tasks.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports.init = function(conf, data) 
+{
+    conf.extendObject(data['js-libs'].libs, {
+        'e-tasks': '../esite/packages/ecore/Tasks/js-libs/e-tasks',
+    });   
+}
